50acfa2743ac3f5324429c1ee529d4104fd0fb16
eunji1 <ssak1088@gmail.com>
2022-06-01 15:25:13 +0000

    try:except

--- video/faceRecognitionFromVideo.py
+++ video/faceRecognitionFromVideo.py
@@ -26,25 +26,29 @@ with open ("./videos.json","r") as loadJson:
                 while True:
                     success, image = v_cap.read()  # BGR
                     print("OK")
-                    if success and int(v_cap.get(1)) == 1:
-                    #if success :
-                        boxes = detector(image)
-                        box = boxes[0]
-                        print("box: ", box)
-                        face = image[box.top():box.bottom(), box.left():box.right()]
-                        #print(face)
-                        PATH = mp4_path.replace('video', 'face_video')
-                        #cv2.imwrite(os.path.join(PATH, '%d.png' % v_cap.get(1)), face)
-                        
-                        # 너비 , 높이, 수평위치, 수직위치
-                        call('ffmpeg -i {} -vf "crop={}:{}:{}:{}" {}'.format(mp4_path, 
-                                                                            box.bottom()-box.top(),
-                                                                            box.right()-box.left(),
-                                                                            box.left(),
-                                                                            box.top(),
-                                                                            PATH), shell =True)
-                    else:
-                        break
+                    try:
+                        if success and int(v_cap.get(1)) == 1:
+                        #if success :
+                            boxes = detector(image)
+                            box = boxes[0]
+                            print("box: ", box)
+                            face = image[box.top():box.bottom(), box.left():box.right()]
+                            #print(face)
+                            PATH = mp4_path.replace('video', 'face_video')
+                            #cv2.imwrite(os.path.join(PATH, '%d.png' % v_cap.get(1)), face)
+                            
+                            # 너비 , 높이, 수평위치, 수직위치
+                            call('ffmpeg -i {} -vf "crop={}:{}:{}:{}" {}'.format(mp4_path, 
+                                                                                box.bottom()-box.top(),
+                                                                                box.right()-box.left(),
+                                                                                box.left(),
+                                                                                box.top(),
+                                                                                PATH), shell =True)
+                        else:
+                            break
+                            #얼굴 못찾는 영상 넘기기
+                    except IndexError:
+                        print("error")
 
 
 '''
